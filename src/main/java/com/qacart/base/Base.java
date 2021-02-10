8d043b67cf87253a49e9a22df956b48619a83dfb
HoussemDev <houss.sword@gmail.com>
2021-02-10 18:07:45 +0100

    [Add] Listner and login testcase

--- src/main/java/com/qacart/base/Base.java
+++ src/main/java/com/qacart/base/Base.java
@@ -1,31 +1,72 @@
 package com.qacart.base;
 
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.PageFactory;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Parameters;
+import org.testng.ITestResult;
+import org.testng.annotations.*;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.lang.reflect.Method;
 import java.net.URL;
+import java.security.PublicKey;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class Base {
 
-    public Base(){
-        PageFactory.initElements(new AppiumFieldDecorator(driver), this);
-    }
+
 
     protected static AndroidDriver<MobileElement> driver;
     protected FileInputStream inputStream;
     protected Properties prop;
+    public static ExtentReports extent;
+    public static ExtentTest logger;
+
+    public Base(){
+        PageFactory.initElements(new AppiumFieldDecorator(driver), this);
+    }
+
+    @BeforeSuite
+    public void beforeSuite(){
+        extent = new ExtentReports("Reports/index.html");
+        extent.addSystemInfo("Framework Type", "Appium Page Object");
+        extent.addSystemInfo("Author","Houssem");
+        extent.addSystemInfo("Environment", "Linux");
+        extent.addSystemInfo("App0", "To Do QACart");
+
+    }
+
+    @BeforeMethod
+    public void beforeMethod(Method method){
+        logger = extent.startTest(method.getName());
+    }
+
+    @AfterSuite
+    public void afterSuite(){
+        extent.flush();
+    }
+
+    @AfterMethod
+    public void afterMethod(Method method, ITestResult result){
+        if(result.getStatus() == ITestResult.SUCCESS){
+            logger.log(LogStatus.PASS, "Test is passed because there is no error");
+        }else if(result.getStatus() == ITestResult.FAILURE){
+            logger.log(LogStatus.FAIL, "Test is Failed");
+            logger.log(LogStatus.FAIL, result.getThrowable());
+
+        }else {
+            logger.log(LogStatus.SKIP, "Test is skipped");
+        }
+    }
 
     @Parameters({"deviceName", "platformName", "platformVersion"})
     @BeforeClass
